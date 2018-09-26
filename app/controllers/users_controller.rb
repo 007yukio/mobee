2a898295d06ef617d2fad6939574ad2a72a2bfb0
007 <tenten0407team@yahoo.co.jp>
2018-09-26 09:26:35 +0000

    完成ver-1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id)
+      #login画面に遷移
+      redirect_to new_session_path(@user.id)
     else
       render 'new'
     end
